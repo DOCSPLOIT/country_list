62f78055f446a42c602c870e0d01fd1c360d909f
docsploit <robertdevasia@outlook.com>
2022-11-14 20:45:57 +0530

    dark and responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,28 @@ import { Component } from "@angular/core";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-
+  isDarkMode = false
+  constructor() {
+    // const document = document.getElementsByTagName('html')[0]
+    this.getDarkMode();
+  }
+  onChange() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      localStorage.setItem('theme', 'dark')
+    } else {
+      localStorage.setItem('theme', 'light')
+    }
+    this.getDarkMode();
+  }
+  getDarkMode() {
+    if (localStorage['theme'] === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark');
+      this.isDarkMode = true
+    } else {
+      document.documentElement.classList.remove('dark')
+      this.isDarkMode = false
+    }
+  }
 
 }
